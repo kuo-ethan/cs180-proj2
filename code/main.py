f4dc60c0d5911afab48f15b85bda6c8ad098dd21
Ethan Kuo <ek0335@berkeley.edu>
2024-09-23 20:36:31 -0700

    finished up to multiresolution blending

--- code/main.py
+++ code/main.py
@@ -1,7 +1,8 @@
-from scipy.signal import convolve2d
 import numpy as np
 import skimage.io as skio
-from utils import save_image, gaussian_blur, gradient_edge, gradient_edge_fast, sharpen, sharpen_fast
+from skimage import color
+from utils import save_image, gaussian_blur, gradient_edge, \
+gradient_edge_fast, sharpen, sharpen_fast, align_images, hybrid
 
 # ======== PART 1: Filters ========
 
@@ -32,3 +33,72 @@ save_image(sharpened_image, 'taj_sharpened:unsharp_mask')
 # Do the same by convolving the image with the unsharp mask filter (optimization)
 sharpened_image = sharpen_fast(taj_image)
 save_image(sharpened_image, 'taj_sharpened:unsharp_mask_filter')
+
+# Sharpen my own images
+image = skio.imread(f'../data/cookie.jpg')
+sharpened_image = sharpen_fast(image, alpha=5)
+save_image(sharpened_image, 'cookie_sharpened')
+
+image = skio.imread(f'../data/pug.jpg')
+sharpened_image = sharpen_fast(image, alpha=5)
+save_image(sharpened_image, 'pug_sharpened')
+
+image = skio.imread(f'../data/bros.jpg')
+sharpened_image = sharpen_fast(image, alpha=15)
+save_image(sharpened_image, 'bros_sharpened')
+
+# Blur then sharpen an image
+image = skio.imread(f'../data/me.jpg')
+blurred_image = gaussian_blur(image, ksize=10, sigma=2)
+save_image(blurred_image, 'me_blurred')
+sharpened_image = sharpen_fast(blurred_image, alpha=10)
+save_image(sharpened_image, 'me_blurred_then_sharpened')
+
+# ==== 2.2: Hybrid Images ====
+# Dereck and nutmeg
+im1 = skio.imread(f'../data/nutmeg.jpg')
+im2 = skio.imread(f'../data/derek.jpg')
+im1, im2 = align_images(im1, im2)
+hybrid_im = hybrid(im1, im2, 25, 16)
+save_image(hybrid_im, 'derek_nutmeg_hybrid')
+
+# Gojo and Levi (also show images in Fourier domain)
+im1 = skio.imread(f'../data/levi.jpg')[:,:,:3]
+im2 = skio.imread(f'../data/gojo.jpg')[:,:,:3]
+im1, im2 = align_images(im1, im2)
+gray_im1, gray_im2 = color.rgb2gray(im1), color.rgb2gray(im2)
+
+cutoff_freq = (25, 16)
+high_freq_im1 = gray_im1 - gaussian_blur(gray_im1, cutoff_freq[0], cutoff_freq[1])
+low_freq_im2 = gaussian_blur(gray_im2, cutoff_freq[0], cutoff_freq[1])
+hybrid_im = np.clip(low_freq_im2 + high_freq_im1, 0, 1)
+save_image(hybrid_im, 'levi_gojo_hybrid')
+
+save_image(np.log(np.abs(np.fft.fftshift(np.fft.fft2(gray_im1)))), 'levi_fourier')
+save_image(np.log(np.abs(np.fft.fftshift(np.fft.fft2(gray_im2)))), 'gojo_fourier')
+save_image(np.log(np.abs(np.fft.fftshift(np.fft.fft2(high_freq_im1)))), 'levi_high_freq_fourier')
+save_image(np.log(np.abs(np.fft.fftshift(np.fft.fft2(low_freq_im2)))), 'gojo_low_freq_fourier')
+save_image(np.log(np.abs(np.fft.fftshift(np.fft.fft2(hybrid_im)))), 'levi_gojo_hybrid_fourier')
+
+# Young and old Ethan
+im1 = skio.imread(f'../data/young.PNG')[:,:,:3]
+im2 = skio.imread(f'../data/old.PNG')[:,:,:3]
+im1, im2 = align_images(im1, im2)
+hybrid_im = hybrid(im1, im2, 40, 30)
+save_image(hybrid_im, 'ethan_hybrid')
+
+# MJ and Kobe fadeaways (Kobe colored)
+im1 = skio.imread(f'../data/kobe.png')[:,:,:3]
+im2 = skio.imread(f'../data/mj.png')[:,:,:3]
+im1, im2 = align_images(im1, im2)
+
+cutoff_freq = (15, 10)
+color_im1, gray_im2 = im1, color.rgb2gray(im2)
+low_freq_im2 = gaussian_blur(gray_im2, cutoff_freq[0], cutoff_freq[1])
+low_freq_im2 = np.dstack([low_freq_im2] * 3)  # Convert grayscale to 3-channel
+low_freq_im1 = gaussian_blur(color_im1, cutoff_freq[0], cutoff_freq[1])
+high_freq_im1 = color_im1 - low_freq_im1
+hybrid_im = np.clip(low_freq_im2 + high_freq_im1, 0, 1)
+save_image(hybrid_im, 'kobe_mj_hybrid')
+
+# ==== 2.2: Gaussian and Laplacian Stacks ====
--- code/utils.py
+++ code/utils.py
@@ -2,11 +2,18 @@ import numpy as np
 import skimage.io as skio
 import cv2
 from scipy.signal import convolve2d
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage.transform as sktr
+from skimage import color
 
-# PARAMETERS AND CONSTANTS
+# ========== Parameters ============
 DEFAULT_GAUSSIAN_KSIZE = 5
 DEFAULT_GAUSSIAN_SD = 1
 DEFAULT_GRADIENT_THRESHOLD = 0.3
+
+# ========== Helper functions ============
 
 # Normalize and save images, automatically accounting for grayscale or RGB
 def save_image(image, filename):
@@ -94,7 +101,7 @@ def sharpen_fast(image, ksize=DEFAULT_GAUSSIAN_KSIZE, sigma=DEFAULT_GAUSSIAN_SD,
     for i in range(3):
         sharpened_image.append(convolve2d(image[:, :, i], unsharp_mask_filter, mode='same'))
     sharpened_image = np.dstack([channel for channel in sharpened_image])
-    return np.clip(sharpened_image, 0, 255).astype(np.uint8) 
+    return np.clip(sharpened_image, 0, 255).astype(np.uint8)
 
 # Function to create the identity filter. Size should be odd.
 def create_identity_filter(size):
@@ -102,3 +109,89 @@ def create_identity_filter(size):
     center = size // 2
     identity_filter[center, center] = 1
     return identity_filter
+
+# Return a hybrid of two aligned images
+def hybrid(im1, im2, cutoff_ksize, cutoff_sigma):
+    gray_im1, gray_im2 = color.rgb2gray(im1), color.rgb2gray(im2)
+    high_freq_im1 = gray_im1 - gaussian_blur(gray_im1, cutoff_ksize, cutoff_sigma)
+    low_freq_im2 = gaussian_blur(gray_im2, cutoff_ksize, cutoff_sigma)
+    return np.clip(low_freq_im2 + high_freq_im1, 0, 1)
+
+# ========== Starter code functions ============
+def get_points(im1, im2):
+    print('Please select 2 points in each image for alignment.')
+    plt.imshow(im1)
+    p1, p2 = plt.ginput(2)
+    plt.close()
+    plt.imshow(im2)
+    p3, p4 = plt.ginput(2)
+    plt.close()
+    return (p1, p2, p3, p4)
+
+def recenter(im, r, c):
+    R, C, _ = im.shape
+    rpad = (int) (np.abs(2*r+1 - R))
+    cpad = (int) (np.abs(2*c+1 - C))
+    return np.pad(
+        im, [(0 if r > (R-1)/2 else rpad, 0 if r < (R-1)/2 else rpad),
+             (0 if c > (C-1)/2 else cpad, 0 if c < (C-1)/2 else cpad),
+             (0, 0)], 'constant')
+
+def find_centers(p1, p2):
+    cx = np.round(np.mean([p1[0], p2[0]]))
+    cy = np.round(np.mean([p1[1], p2[1]]))
+    return cx, cy
+
+def align_image_centers(im1, im2, pts):
+    p1, p2, p3, p4 = pts
+    h1, w1, b1 = im1.shape
+    h2, w2, b2 = im2.shape
+    
+    cx1, cy1 = find_centers(p1, p2)
+    cx2, cy2 = find_centers(p3, p4)
+
+    im1 = recenter(im1, cy1, cx1)
+    im2 = recenter(im2, cy2, cx2)
+    return im1, im2
+
+def rescale_images(im1, im2, pts):
+    p1, p2, p3, p4 = pts
+    len1 = np.sqrt((p2[1] - p1[1])**2 + (p2[0] - p1[0])**2)
+    len2 = np.sqrt((p4[1] - p3[1])**2 + (p4[0] - p3[0])**2)
+    dscale = len2/len1
+    if dscale < 1:
+        im1 = sktr.rescale(im1, (dscale, dscale, 1)) # should be same as passing multichannel=True
+    else:
+        im2 = sktr.rescale(im2, (1./dscale, 1./dscale, 1))
+    return im1, im2
+
+def rotate_im1(im1, im2, pts):
+    p1, p2, p3, p4 = pts
+    theta1 = math.atan2(-(p2[1] - p1[1]), (p2[0] - p1[0]))
+    theta2 = math.atan2(-(p4[1] - p3[1]), (p4[0] - p3[0]))
+    dtheta = theta2 - theta1
+    im1 = sktr.rotate(im1, dtheta*180/np.pi)
+    return im1, dtheta
+
+def match_img_size(im1, im2):
+    # Make images the same size
+    h1, w1, c1 = im1.shape
+    h2, w2, c2 = im2.shape
+    if h1 < h2:
+        im2 = im2[int(np.floor((h2-h1)/2.)) : -int(np.ceil((h2-h1)/2.)), :, :]
+    elif h1 > h2:
+        im1 = im1[int(np.floor((h1-h2)/2.)) : -int(np.ceil((h1-h2)/2.)), :, :]
+    if w1 < w2:
+        im2 = im2[:, int(np.floor((w2-w1)/2.)) : -int(np.ceil((w2-w1)/2.)), :]
+    elif w1 > w2:
+        im1 = im1[:, int(np.floor((w1-w2)/2.)) : -int(np.ceil((w1-w2)/2.)), :]
+    assert im1.shape == im2.shape
+    return im1, im2
+
+def align_images(im1, im2):
+    pts = get_points(im1, im2)
+    im1, im2 = align_image_centers(im1, im2, pts)
+    im1, im2 = rescale_images(im1, im2, pts)
+    im1, angle = rotate_im1(im1, im2, pts)
+    im1, im2 = match_img_size(im1, im2)
+    return im1, im2
